2fcedca4dad2994c5d88c25d6e60d5d669259bc6
atchuthkumar96 <atchuthkumar96@gmail.com>
2022-02-28 18:52:47 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'app-home', component: HomeComponent },
+  { path: 'edit-book/:id', component: BookDetailComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
